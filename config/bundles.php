ca821a5fce307e3e038d6b8e85107eee75ae1148
Sidney <sidneyfranciscodev@gmail.com>
2025-09-11 17:24:23 +0200

    require doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
